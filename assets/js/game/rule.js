e60888013a00aa91998bd865e18f3d0af40c238a
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2022-09-11 04:30:48 +0200

    Refactored rules

--- assets/js/game/rule.js
+++ assets/js/game/rule.js
@@ -4,9 +4,9 @@ class Rule {
 
         for (let i = 0; i < particles1.length; i++) {
             var force = new Vector(0, 0);
+            var a = particles1[i];
 
             for (let j = 0; j < particles2.length; j++) {
-                var a = particles1[i];
                 var b = particles2[j];
 
                 const delta = new Vector(
